c2533b431ba9f813bf023ae4ccac62b0223bd9ba
yeetnbeet <segalcsam@gmail.com>
2023-01-12 13:52:22 -0700

    init

---
+++ app.py
@@ -0,0 +1,47 @@
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory 
+from werkzeug.utils import secure_filename
+from helpers import create_output
+
+UPLOAD_FOLDER = './files'
+ALLOWED_EXTENSIONS = {'csv'}
+
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = "ghasdfalskdjhfaklsdf"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        if 'file2' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        file2 = request.files['file2']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == ''or file2.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and file2 and allowed_file(file.filename) and allowed_file(file2.filename):
+            filename = secure_filename(file.filename)
+            filename2 = secure_filename(file2.filename)
+            path1 = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            path2 = os.path.join(app.config['UPLOAD_FOLDER'], filename2)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file2.save((os.path.join(app.config['UPLOAD_FOLDER'], filename2)))
+            create_output(path1,path2)
+            return redirect(url_for('download_file', name="OUTPUT.csv"))
+    return render_template("index.html")
+@app.route('/uploads/<name>')
+def download_file(name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], name)
---
+++ helpers.py
@@ -0,0 +1,63 @@
+import csv ;
+
+#config is the object that contains file locations and data for manipulations
+class config :
+    def __init__(self, builderFile = "./BUILDER.csv",framesFile = "./FRAMES.csv",outputFile = "./files/OUTPUT.csv"):
+        self.builderFile = builderFile
+        self.framesFile = framesFile
+        self.outputFile = outputFile
+    Aframes = []
+    Abuilders = []
+    fields = []
+
+def dataINIT(config):
+    builderCSV = config.builderFile
+    framesCSV = config.framesFile  
+    
+
+    with open(framesCSV, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        config.fields = next(csvreader)
+        for row in csvreader:
+            config.Aframes.append(row)
+    
+    with open(builderCSV, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        Bfields = next(csvreader)
+        for row in csvreader:
+            config.Abuilders.append(row)
+
+    return 
+
+def syncronize(config):
+    Aframes = config.Aframes
+    Abuilders = config.Abuilders
+    for i in Abuilders:
+        for r in Aframes:
+            if r[8] == i[8]:
+                if r[11] != i[11]:
+                    print("*Triggered* PC SKU: ",i[8]," Changed: ",i[11]," to: ",r[11])
+                    i[11] = r[11]    
+                if r[12] != i[12]:
+                    print("*Triggered* SLC SKU: ",i[8]," Changed: ",i[12]," to: ",r[12])
+                    i[12] = r[12]
+                if r[13] != i[13]:
+                    print("*Triggered* WH SKU: ",i[8]," Changed: ",i[13]," to: ",r[13])
+                    i[13] = r[13]
+
+def output(config):
+    outputCSV = config.outputFile
+    with open(outputCSV,mode='w') as csvFile:
+        writer = csv.writer(csvFile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(config.fields)
+        for i in config.Abuilders:
+            writer.writerow(i)
+
+
+def create_output (filepath1,filepath2):
+    C = config(builderFile=filepath1,framesFile=filepath2)
+    dataINIT(C)
+    print("\nTEST_LOGS:\n")
+    syncronize(C)
+    output(C)
+    print("\n\n\n")
